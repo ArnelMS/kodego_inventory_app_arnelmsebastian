b82d2ada6be69adc648c2ad78f93ef0be40e00d0
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-10-15 01:07:24 +0800

    Oct1 2ND commit

--- app/src/main/java/com/kodego/activity/app/inventoryappsebastian/HomeActivity.kt
+++ app/src/main/java/com/kodego/activity/app/inventoryappsebastian/HomeActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_home2)
     }
 }
