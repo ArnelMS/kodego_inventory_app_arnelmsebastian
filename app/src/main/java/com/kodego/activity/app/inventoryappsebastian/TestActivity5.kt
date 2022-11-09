b01597c083a104abd296cb2b20b33a93589d2541
Arnel Sebastian <arnelmsebastian@gmail.com>
2022-11-09 20:20:56 +0800

    Test Aactivity 5

--- app/src/main/java/com/kodego/activity/app/inventoryappsebastian/TestActivity5.kt
+++ app/src/main/java/com/kodego/activity/app/inventoryappsebastian/TestActivity5.kt
@@ -19,32 +19,32 @@ import com.kodego.activity.app.inventoryappsebastian.databinding.ActivityTest5Bi
 
 class TestActivity5 : AppCompatActivity() {
 
-    private lateinit var binding : ActivityTest5Binding
+    private lateinit var binding: ActivityTest5Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTest5Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCamera2.setOnClickListener(){
+        binding.btnCamera2.setOnClickListener() {
             showCamera()
         }
     }
 
-    private fun showCamera(){
+    private fun showCamera() {
         Dexter.withContext(this).withPermission(
             Manifest.permission.CAMERA
-        ).withListener(object: PermissionListener{
+        ).withListener(object : PermissionListener {
             override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
                 val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 //                startActivity(cameraIntent) // only used to access camera
 
                 cameraLauncher.launch(cameraIntent)
-                Toast.makeText(applicationContext,"Permission Granted", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
             }
 
             override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
-                Toast.makeText(applicationContext,"Permission Granted", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_SHORT).show()
             }
 
             override fun onPermissionRationaleShouldBeShown(
@@ -56,14 +56,16 @@ class TestActivity5 : AppCompatActivity() {
 
         }).onSameThread().check()
     }
-    val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
-        if (result.resultCode== Activity.RESULT_OK){
-            result.data?.extras.let{
-                val image : Bitmap = result.data?.extras?.get("data")as Bitmap
-                binding.imgTestProfile.setImageBitmap(image)
+
+    val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                result.data?.extras.let {
+                    val image: Bitmap = result.data?.extras?.get("data") as Bitmap
+                    binding.imgTestProfile.setImageBitmap(image)
+
+                }
 
             }
         }
-    }
-
 }
